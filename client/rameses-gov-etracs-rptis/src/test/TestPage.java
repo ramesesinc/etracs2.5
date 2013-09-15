3853710cb35daaaabe64d94a092d52350a8c7e57
Jessie S. Zamora <jzamss@gmail.com>
2013-09-15 08:35:34 +0800

    Add Tester

--- client/rameses-gov-etracs-rptis/src/test/TestPage.java
+++ client/rameses-gov-etracs-rptis/src/test/TestPage.java
@@ -25,10 +25,14 @@ public class TestPage extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc=" Generated Code ">//GEN-BEGIN:initComponents
     private void initComponents() {
         xButton1 = new com.rameses.rcp.control.XButton();
+        xDecimalField1 = new com.rameses.rcp.control.XDecimalField();
 
         xButton1.setCaption("Invoke");
         xButton1.setName("fire");
         xButton1.setText("Fire");
+
+        xDecimalField1.setName("amount");
+        xDecimalField1.setScale(6);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
@@ -38,19 +42,26 @@ public class TestPage extends javax.swing.JPanel {
                 .addContainerGap()
                 .addComponent(xButton1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(339, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(191, Short.MAX_VALUE)
+                .addComponent(xDecimalField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(109, 109, 109))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(xButton1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(266, Short.MAX_VALUE))
+                .addGap(62, 62, 62)
+                .addComponent(xDecimalField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(184, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
     
     
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private com.rameses.rcp.control.XButton xButton1;
+    private com.rameses.rcp.control.XDecimalField xDecimalField1;
     // End of variables declaration//GEN-END:variables
     
 }
