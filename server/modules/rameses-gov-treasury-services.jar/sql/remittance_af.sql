3c7be7e12b6932035d10d58778b1e625ce047d05
Arnel Retiza <retiza.arnel@gmail.com>
2013-10-24 18:00:59 +0800

    Remittance_af: afcontrol will not forward to next remittance if already consumed

--- server/modules/rameses-gov-treasury-services.jar/sql/remittance_af.sql
+++ server/modules/rameses-gov-treasury-services.jar/sql/remittance_af.sql
@@ -126,7 +126,9 @@ SELECT
   MAX(ad.endingendseries) AS endseries
 FROM afserial_inventory_detail ad 
 INNER JOIN remittance_afserial af ON ad.objid=af.objid
+INNER JOIN afserial_inventory ai on ai.objid = ad.controlid 
 WHERE af.remittanceid = $P{remittanceid}
+  AND ai.currentseries <= ai.endseries
 GROUP BY ad.controlid
 
 [getCashTicketRemittanceForBalanceForward]
@@ -137,7 +139,7 @@ SELECT DISTINCT
 FROM cashticket_inventory_detail ct 
 INNER JOIN cashticket_inventory cti ON ct.controlid=cti.objid 
 INNER JOIN remittance_cashticket rc ON ct.objid=rc.objid
-WHERE rc.remittanceid=$P{remittanceid}
+WHERE rc.remittanceid=$P{remittanceid} and cti.qtybalance > 0
 GROUP BY ct.controlid
 
 [getRemittedAFSerial]
