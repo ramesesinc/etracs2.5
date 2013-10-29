f37d1d6c04e7a8f8e0c86abd216fbe879fed1988
elmo <elmonazareno@gmail.com>
2013-10-29 14:09:36 +0800

    alter table for rules

---
+++ migration/rule-mgmt/alter-rule-tables.sql
@@ -0,0 +1,18 @@
+ALTER TABLE sys_rule_condition ADD dynamic_datatype varchar(50);
+ALTER TABLE sys_rule_condition ADD dynamic_key varchar(50);
+ALTER TABLE sys_rule_condition ADD dynamic_value varchar(50); 
+
+
+
+ALTER TABLE sys_rule_fact ADD dynamic int;
+ALTER TABLE sys_rule_fact ADD lookuphandler varchar(50);
+ALTER TABLE sys_rule_fact ADD lookupkey varchar(50);
+ALTER TABLE sys_rule_fact ADD lookupvalue varchar(50);
+ALTER TABLE sys_rule_fact ADD lookupdatatype varchar(50);
+ALTER TABLE sys_rule_fact ADD dynamicfieldname varchar(50);
+ALTER TABLE sys_rule_fact ADD builtinconstraints varchar(50);
+
+
+ALTER TABLE sys_rule_condition_constraint DROP COLUMN dynamicvar_objid;
+ALTER TABLE sys_rule_condition_constraint DROP COLUMN dynamicvar_name;
+ALTER TABLE sys_rule_condition_constraint DROP COLUMN dynamicvar_datatype;
