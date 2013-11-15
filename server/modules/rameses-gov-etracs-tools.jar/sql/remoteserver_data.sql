11116e9ee5783bd95da7bf1b8acee6fe61308c68
Jessie S. Zamora <jzamss@gmail.com>
2013-11-15 12:12:39 +0800

    Export funds

--- server/modules/rameses-gov-etracs-tools.jar/sql/remoteserver_data.sql
+++ server/modules/rameses-gov-etracs-tools.jar/sql/remoteserver_data.sql
@@ -11,6 +11,9 @@ SELECT * FROM sys_usergroup_member WHERE user_objid = $P{userid}
 [getUserCashBooks]
 SELECT * FROM cashbook WHERE subacct_objid = $P{userid}
 
+[getFunds]
+SELECT * FROM fund
+
 
 [insertUserMembership]
 INSERT INTO sys_usergroup_member(
@@ -45,4 +48,25 @@ VALUES (
   ,$P{displayname}
   ,$P{jobtitle}
   ,$P{usertxncode}
+)
+
+
+[insertFund]
+INSERT INTO fund(
+   objid
+  ,parentid
+  ,state
+  ,code
+  ,title
+  ,type
+  ,special
+)
+VALUES (
+  $P{objid}
+  ,$P{parentid}
+  ,$P{state}
+  ,$P{code}
+  ,$P{title}
+  ,$P{type}
+  ,$P{special}
 )
