fd02b724bfec8e19ceefe5588119136329954cbc
Jessie S. Zamora <jzamss@gmail.com>
2013-11-20 19:11:23 +0800

    Subcollector Remittance DCR Iligan Format

--- resource/db_updates/20131114 - Subcollector Cashbreakdown support.sql
+++ resource/db_updates/20131114 - Subcollector Cashbreakdown support.sql
@@ -5,6 +5,8 @@ alter table subcollector_remittance add totalnoncash numeric(12,2)
 go
 alter table subcollector_remittance add cashbreakdown varchar(800)
 go
+alter table subcollector_remittance add collectionsummaries text
+go
 
 update subcollector_remittance set 
 	totalcash = 0.0,
