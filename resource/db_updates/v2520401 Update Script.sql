239780dd1247368ec36a9bdc0a73ffd32ec46e57
Jessie S. Zamora <jzamss@gmail.com>
2013-11-26 16:59:54 +0800

    Patch 2.5.204.01 : Add deposit_collection_by_bank_account option to sys_var

---
+++ resource/db_updates/v2520401 Update Script.sql
@@ -0,0 +1,3 @@
+
+insert into sys_var(name, value, description, datatype, category)
+values ('deposit_collection_by_bank_account', '0', 'Deposit collection by bank account instead of by fund', 'checkbox', 'TC')
