b7bfdfd924c2e1bd2dc4e1ca32cda77d66140c41
worgflores <worgflores@gmail.com>
2013-12-09 13:08:19 +0800

    updated the etracs_platform executable files

--- platform/etracs2_5_platform_thin/etracs_platform.bat
+++ platform/etracs2_5_platform_thin/etracs_platform.bat
@@ -1,7 +1,7 @@
 @echo off
 
 rem Specify the java home directory 
-rem set JAVA_HOME=C:\Program Files\Java\jdk1.7.0_45
+rem set JAVA_HOME=C:\Program Files\Java\jdk1.6.0_17
 
 rem Specify some java options
 set JAVA_OPTS=-Xmx512m
@@ -22,5 +22,5 @@ echo   JAVA        : %JAVA%
 echo.
 echo.========================================================================
 
-"%JAVA%" %JAVA_OPTS% -cp lib/*;. com.rameses.osiris3.platform.OSBootStrap
+"%JAVA%" %JAVA_OPTS% -cp lib/ext/%PROCESSOR_ARCHITECTURE%;lib/*;. com.rameses.osiris3.platform.OSBootStrap
 pause
