2edd2283d646e8b19ea9dde923d3b7f498ae3a10
Jessie S. Zamora <jzamss@gmail.com>
2013-12-14 18:15:52 +0800

    RPT Collection update using modified billing process

--- resource/db_updates/v25205-001 RPTBilling Improvement Scripts.sql
+++ resource/db_updates/v25205-001 RPTBilling Improvement Scripts.sql
@@ -1,4 +1,50 @@
 
+
+alter table rptledger add lastbilledyear integer default 0
+go
+
+alter table rptledger add lastbilledqtr integer default 0
+go
+
+alter table rptledger add partialbasic numeric(16,2) default 0.0
+go
+
+alter table rptledger add partialbasicint numeric(16,2) default 0.0
+go
+
+alter table rptledger add partialbasicdisc numeric(16,2) default 0.0
+go
+
+alter table rptledger add partialsef numeric(16,2) default 0.0
+go
+
+alter table rptledger add partialsefint numeric(16,2) default 0.0
+go
+
+alter table rptledger add partialsefdisc numeric(16,2) default 0.0
+go
+
+alter table rptledger add partialledyear int default 0
+go
+
+alter table rptledger add partialledqtr int default 0
+go
+
+
+update rptledger set 
+	lastbilledyear = 0,
+	lastbilledqtr = 0,
+	partialbasic = 0,
+	partialbasicint = 0,
+	partialbasicdisc = 0,
+	partialsef = 0,
+	partialsefint = 0,
+	partialsefdisc = 0,
+	partialledyear = 0,
+	partialledqtr = 0
+go	
+
+
 drop table rptledgerbillitem
 go
 
@@ -39,6 +85,7 @@ CREATE TABLE rptledgerbillitem(
 	[amnestyinfo] [varchar](50) NULL,
 	[forpayment] [int] NOT NULL,
 	[partial] [int] NULL,
+	[partialapplied] [int] NULL,
 	[partialbasic] [decimal](10, 2) NULL,
 	[partialbasicint] [decimal](10, 2) NULL,
 	[partialbasicdisc] [decimal](10, 2) NULL,
@@ -74,3 +121,44 @@ go
 
 create index ix_rptledgerbillitem_rptledgerfaasid on rptledgerbillitem(rptledgerfaasid)
 go
+
+
+alter table rptledgerbillitem add constraint ux_rptledgerbillitem_ledgeridyearqtr unique(rptledgerid, year,qtr)
+go
+
+
+
+
+alter table cashreceiptitem_rpt drop constraint FK_cashreceiptrptitem_rptledgeritem
+go
+
+
+drop index cashreceiptitem_rpt.FK_cashreceiptrptitem_rptledgeritemqtrly
+go
+
+drop index cashreceiptitem_rpt.FK_rptreceiptitem_rptledgeritem
+go
+
+alter table cashreceiptitem_rpt drop column rptledgeritemid
+go
+
+alter table cashreceiptitem_rpt drop constraint DF__cashrecei__rptle__10966653
+go
+
+alter table cashreceiptitem_rpt drop column rptledgeritemqtrlyid
+go
+
+alter table cashreceiptitem_rpt drop column qtrly 
+go
+
+
+
+alter table cashreceiptitem_rpt add rptledgerfaasid varchar(50)
+go
+alter table cashreceiptitem_rpt add fromqtr int
+go
+alter table cashreceiptitem_rpt add toqtr int
+go
+
+
+
--- server/modules/rameses-gov-etracs-rptis-services.jar/sql/rptbill_mssql.sql
+++ server/modules/rameses-gov-etracs-rptis-services.jar/sql/rptbill_mssql.sql
@@ -11,6 +11,8 @@ SELECT
 	rl.qtrlypaymentpaidontime,
 	rl.lastitemyear,
 	rl.faasid, 
+	rl.lastbilledyear,
+	rl.lastbilledqtr,
 	f.tdno,
 	f.owner_name,
 	f.administrator_name,
@@ -23,8 +25,15 @@ SELECT
 	rp.cadastrallotno,
 	rp.barangayid,
 	pc.code AS classcode,
-	CASE WHEN rl.lastqtrpaid = 4 THEN rl.lastyearpaid + 1 ELSE rl.lastyearpaid END AS fromyear,
-	CASE WHEN rl.lastqtrpaid = 4 THEN 1 ELSE rl.lastqtrpaid + 1 END AS fromqtr
+	CASE WHEN rl.partialbasic > 0 THEN 1 ELSE 0 END AS partialled,
+	CASE
+		WHEN rl.partialbasic > 0 THEN rl.partialledyear  
+		WHEN rl.lastqtrpaid = 4 THEN rl.lastyearpaid + 1 ELSE rl.lastyearpaid 
+	END	AS fromyear,
+	CASE
+		WHEN rl.partialbasic > 0 THEN rl.partialledqtr
+		WHEN rl.lastqtrpaid = 4 THEN 1 ELSE rl.lastqtrpaid + 1 
+	END AS fromqtr
 FROM faas f
 	INNER JOIN rpu r ON f.rpuid = r.objid 
 	INNER JOIN propertyclassification pc ON r.classification_objid = pc.objid 
@@ -48,7 +57,9 @@ WHERE rl.objid =  $P{rptledgerid}
 
 [updateLedgerNextBillDate]
 UPDATE rptledger SET
-	nextbilldate = $P{nextbilldate}
+	nextbilldate = $P{nextbilldate},
+	lastbilledyear = $P{toyear},
+	lastbilledqtr = $P{toqtr}
 WHERE objid = $P{rptledgerid}
 
 
@@ -67,14 +78,9 @@ SELECT
 	rl.objid,
 	rl.lastyearpaid,
 	rl.lastqtrpaid,
-	rl.firstqtrpaidontime,
-	rl.qtrlypaymentpaidontime,
-	rl.lastitemyear,
 	rl.faasid, 
 	rl.nextbilldate,
 	f.tdno,
-	f.owner_name,
-	f.administrator_name,
 	r.rputype,
 	r.fullpin,
 	r.totalareaha,
@@ -83,9 +89,7 @@ SELECT
 	b.name AS barangay,
 	rp.cadastrallotno,
 	rp.barangayid,
-	pc.code AS classcode,
-	CASE WHEN rl.lastqtrpaid = 4 THEN rl.lastyearpaid + 1 ELSE rl.lastyearpaid END AS fromyear,
-	CASE WHEN rl.lastqtrpaid = 4 THEN 1 ELSE rl.lastqtrpaid + 1 END AS fromqtr
+	pc.code AS classcode
 FROM rptledger rl 
 	INNER JOIN faas f ON rl.faasid = f.objid 
 	INNER JOIN rpu r ON f.rpuid = r.objid 
@@ -98,3 +102,80 @@ WHERE ${filters}
  AND ( rl.lastyearpaid < $P{billtoyear} OR ( rl.lastyearpaid = $P{billtoyear} AND rl.lastqtrpaid <= $P{billtoqtr}))
 ORDER BY f.tdno  
 
+
+
+[getFullyPaidItems]
+SELECT 
+	bi.objid AS objid,
+	rl.objid AS rptledgerid,
+	bi.year, 
+	bi.qtr AS qtr,
+	bi.fromqtr,
+	CASE WHEN bi.qtr = 0 THEN bi.toqtr ELSE bi.qtr END AS toqtr,
+	CASE WHEN bi.qtr = 0 THEN 0 ELSE 1 END AS qtrly,
+	rl.lastyearpaid,
+	rl.lastqtrpaid,
+	bi.basic - bi.basicpaid AS basic,
+	bi.basicdisc,
+	bi.basicamnesty,
+	bi.basicint - bi.basicintpaid  AS basicint,
+	bi.basicintamnesty,
+	bi.sef - bi.sefpaid AS sef,
+	bi.sefdisc,
+	bi.sefamnesty,
+	bi.sefint - bi.sefintpaid AS  sefint,
+	bi.sefintamnesty,
+	bi.firecode - bi.firecodepaid AS firecode,
+	( 
+		bi.basic - bi.basicpaid - bi.basicdisc + bi.basicint - bi.basicintpaid + 
+		bi.sef - bi.sefpaid - bi.sefdisc +  bi.sefint - bi.sefintpaid  +
+		bi.firecode - bi.firecodepaid
+	) AS total
+FROM rptledger rl
+		INNER JOIN rptledgerbillitem bi ON rl.objid = bi.rptledgerid
+WHERE rl.objid = $P{rptledgerid}
+ORDER BY bi.year, bi.qtr 
+
+[deleteUnpartialledQtrlyItems]
+DELETE FROM rptledgerbillitem 
+WHERE rptledgerid = $P{rptledgerid}
+  AND ( year > $P{yeartodelete} OR (year = $P{yeartodelete} AND qtr >= $P{qtrtodelete}))
+
+[deleteUnpartialledYearlyItems]
+DELETE FROM rptledgerbillitem 
+WHERE rptledgerid = $P{rptledgerid}
+  AND year >= $P{yeartodelete}
+
+[updatePartialInfo]
+UPDATE rptledgerbillitem  SET
+	basic = $P{partialbasic},
+	basicint = $P{partialbasicint},
+	basicdisc = $P{partialbasicdisc},
+	sef = $P{partialsef},
+	sefint = $P{partialsefint},
+	sefdisc = $P{partialsefdisc},
+
+	partialbasic = $P{partialbasic},
+	partialbasicint = $P{partialbasicint},
+	partialbasicdisc = $P{partialbasicdisc},
+	partialsef = $P{partialsef},
+	partialsefint = $P{partialsefint},
+	partialsefdisc = $P{partialsefdisc},
+	partial = 1
+WHERE objid = $P{objid}	
+
+
+[applyPartialPayment]
+UPDATE bi SET
+	bi.basicpaid = rl.partialbasic,
+	bi.basicintpaid = rl.partialbasicint,
+	bi.basicdisctaken = ISNULL(rl.partialbasicdisc,0),
+	bi.sefpaid = rl.partialsef,
+	bi.sefintpaid = rl.partialsefint,
+	bi.sefdisctaken = ISNULL(rl.partialsefdisc,0),
+	bi.partialapplied = 1
+FROM rptledger rl 
+	INNER JOIN rptledgerbillitem bi ON rl.objid = bi.rptledgerid
+WHERE rl.objid = $P{objid}
+  AND rl.partialledyear = bi.year
+  AND rl.partialledqtr =  CASE WHEN bi.qtr = 0 THEN bi.fromqtr ELSE bi.qtr END 
