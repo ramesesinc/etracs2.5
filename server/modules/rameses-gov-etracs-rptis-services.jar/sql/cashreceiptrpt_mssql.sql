dbd57d2098f5248cd287a43c9699a2ff21f015b7
Jessie S. Zamora <jzamss@gmail.com>
2014-01-06 15:07:34 +0800

    Add collection facility to total multiple payments by owner

--- server/modules/rameses-gov-etracs-rptis-services.jar/sql/cashreceiptrpt_mssql.sql
+++ server/modules/rameses-gov-etracs-rptis-services.jar/sql/cashreceiptrpt_mssql.sql
@@ -435,3 +435,17 @@ DELETE FROM rptbill WHERE objid = $P{billid}
 
 [deleteRptBillLedgers]
 DELETE FROM rptbill_ledger WHERE rptbillid = $P{billid}
+
+
+[getCollectionsByCount]
+SELECT 
+	cr.receiptno, 
+	CASE WHEN cv.objid IS NULL THEN cr.amount  ELSE 0.0 END AS amount,
+	CASE WHEN cv.objid IS NULL THEN 0  ELSE 1 END AS voided
+FROM cashreceipt cr 
+	LEFT JOIN cashreceipt_void cv ON cr.objid = cv.receiptid 
+	LEFT JOIN remittance_cashreceipt rc ON cr.objid = rc.objid 
+WHERE cr.collector_objid = $P{userid}
+  AND rc.objid IS NULL 
+ORDER BY cr.txndate DESC   
+  
