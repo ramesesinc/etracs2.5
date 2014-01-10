1e24824b220ff38e70c49fb6ecbe2e4c58a74e8a
Jessie S. Zamora <jzamss@gmail.com>
2014-01-10 10:39:25 +0800

    Add Direct To Cash Summary report

--- server/modules/rameses-gov-treasury-services.jar/sql/directcash_collection.sql
+++ server/modules/rameses-gov-treasury-services.jar/sql/directcash_collection.sql
@@ -13,3 +13,16 @@ WHERE dci.parentid = $P{objid}
 
 [approve]
 UPDATE directcash_collection SET state = 'APPROVED' WHERE objid = $P{objid} AND state = 'DRAFT'
+
+
+[getReportData]
+SELECT
+	dc.refno, dc.refdate,
+	ri.code, ri.title, dci.amount
+FROM directcash_collection dc
+	INNER JOIN directcash_collection_item dci ON dc.objid = dci.parentid
+	INNER JOIN revenueitem ri ON dci.item_objid = ri.objid
+WHERE ${filters}
+  AND dc.state = 'APPROVED' 
+  AND ri.objid LIKE $P{acctid}
+ORDER BY dc.refdate , ri.code 
