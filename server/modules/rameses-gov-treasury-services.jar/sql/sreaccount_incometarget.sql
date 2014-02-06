8a04dbfdd82bd4f70f5b5842c1c7d1e66ffae9f8
Arnel Retiza <retiza.arnel@gmail.com>
2014-02-06 13:07:05 +0800

    sreaccount_incometarget: include subaccount for targets

--- server/modules/rameses-gov-treasury-services.jar/sql/sreaccount_incometarget.sql
+++ server/modules/rameses-gov-treasury-services.jar/sql/sreaccount_incometarget.sql
@@ -4,5 +4,5 @@ a.objid, a.code as account_code,
 a.title AS account_title,
 (SELECT TOP 1 target FROM sreaccount_incometarget WHERE objid=a.objid AND year=${year}) AS target 
 FROM sreaccount a 
-WHERE a.type='detail' AND a.code LIKE $P{code} 
-ORDER BY a.code
+WHERE a.type in ('subaccount', 'detail') and a.code LIKE $P{code} 
+ORDER BY a.code 
