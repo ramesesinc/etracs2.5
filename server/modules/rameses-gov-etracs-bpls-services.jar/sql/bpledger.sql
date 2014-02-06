98218dd10f6e0479834763b9887c5360ae8c036c
elmo <elmonazareno@gmail.com>
2014-02-06 15:01:47 +0800

    updated business ledger

--- server/modules/rameses-gov-etracs-bpls-services.jar/sql/bpledger.sql
+++ server/modules/rameses-gov-etracs-bpls-services.jar/sql/bpledger.sql
@@ -27,6 +27,51 @@ WHERE receivableid = $P{objid}
 [removeReceivables]
 DELETE FROM bpreceivable WHERE ledgerid=$P{objid}
 
+###########################################
+# used for billing
+###########################################
+[getUnpaidForBilling]
+SELECT 
+	br.objid AS objid,
+    app.objid AS application_objid,
+    app.appyear AS application_appyear,
+    app.apptype AS application_apptype,
+	app.appno AS application_appno,
+	app.appno,
+
+    bl.objid AS lob_objid,
+    br.lob_objid AS lob_lobid,
+    br.lob_name AS lob_name,
+    l.classification_objid AS lob_classification_objid,
+    bl.assessmenttype AS lob_assessmenttype,
+
+	br.account_objid AS account_objid,
+	br.account_title AS account_title,
+	r.code AS account_code, 
+	ba.taxfeetype AS account_taxfeetype,
+	ba.surcharge_objid AS account_surcharge_objid,
+	ba.interest_objid AS account_interest_objid,
+
+	br.amount, 
+	br.amtpaid, 
+	br.discount,
+	br.refid,
+	br.reftype,
+	br.department,
+	br.iyear, 
+	br.iqtr, 
+	ba.taxfeetype, 
+	br.amount - br.amtpaid - br.discount AS balance
+	
+FROM bpreceivable br 
+INNER JOIN businessaccount ba ON br.account_objid = ba.objid
+INNER JOIN revenueitem r ON  r.objid=ba.objid 
+LEFT JOIN bpapplication app ON app.objid=br.applicationid
+LEFT JOIN lob l ON br.lob_objid=l.objid 
+LEFT JOIN business_lob bl ON bl.applicationid=br.applicationid AND bl.lobid=br.lob_objid
+WHERE br.businessid=$P{businessid} AND ((br.amount-br.amtpaid- br.discount) > 0) 
+ORDER BY br.iyear DESC, br.lob_name DESC, r.code ASC
+
 
 ###########################################
 # used by BPApplicationCashReceiptService
